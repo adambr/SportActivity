5b441e4799a60e44985aa269166817e7d3c9ad2d
Petr Jelinek <petaniss@gmail.com>
2013-11-22 17:34:49 +0100

    funkci injekce, staci napsat @SpringBean, nastaveni se bere ze servisni logiky, DB uz neni inmemmory ale derby v netbeanech

--- sportactivitymanager-web/src/main/java/cz/muni/fi/pa165/sportactivitymanager/web/ActivityActionBean.java
+++ sportactivitymanager-web/src/main/java/cz/muni/fi/pa165/sportactivitymanager/web/ActivityActionBean.java
@@ -4,12 +4,15 @@
  */
 package cz.muni.fi.pa165.sportactivitymanager.web;
 
+import cz.muni.fi.pa165.sportactivitymanager.dto.SportActivityDTO;
+import cz.muni.fi.pa165.sportactivitymanager.service.SportActivityService;
 import java.util.ArrayList;
 import java.util.List;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.integration.spring.SpringBean;
 import net.sourceforge.stripes.validation.ValidationErrorHandler;
 import net.sourceforge.stripes.validation.ValidationErrors;
 
@@ -19,22 +22,28 @@ import net.sourceforge.stripes.validation.ValidationErrors;
  */
 @UrlBinding("/activities/{$event}/{activity.id}")
 public class ActivityActionBean extends BaseActionBean implements ValidationErrorHandler {
+    
+    @SpringBean
+    protected SportActivityService activityService;
+    
+   
+    private List<SportActivityDTO> activities;
 
-    private List<String> activities;
-
-    public List<String> getActivities() {
+    public List<SportActivityDTO> getActivities() {
         return activities;
     }
     
     @DefaultHandler
     public Resolution list() {
-        activities = new ArrayList<String>();
-        activities.add("test1");
-        activities.add("peta");
-        activities.add("adam");
-        activities.add("michal");
-        activities.add("kuba");
+        activities = activityService.findAll();
         return new ForwardResolution("/activity/list.jsp");
+    }
+    
+    public Resolution add() {
+        SportActivityDTO act = new SportActivityDTO();
+        act.setName("Plavání");
+        activityService.create(act);
+        return new ForwardResolution("/activity/add.jsp");
     }
     
     @Override
---
+++ sportactivitymanager-web/src/main/webapp/activity/add.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : add
+    Created on : 22.11.2013, 17:25:34
+    Author     : Petaniss
+--%>
+
+<%@ page contentType="text/html; charset=utf-8" pageEncoding="utf-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="f" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="s" uri="http://stripes.sourceforge.net/stripes.tld" %>
+
+<s:layout-render name="/layout.jsp" titlekey="activities">
+    <s:layout-component name="body">
+        Plavání bylo přidáno.
+        
+    </s:layout-component>
+</s:layout-render>
