13d3126dcad87dbd6bc401b4d36d5bb79b0d7476
tempest <tempest@P0Cat>
2013-11-22 18:45:32 +0100

    pool after petr's

--- sportactivitymanager-web/src/main/java/cz/muni/fi/pa165/sportactivitymanager/web/RecordActionBean.java
+++ sportactivitymanager-web/src/main/java/cz/muni/fi/pa165/sportactivitymanager/web/RecordActionBean.java
@@ -5,6 +5,7 @@
 package cz.muni.fi.pa165.sportactivitymanager.web;
 
 import cz.muni.fi.pa165.sportactivitymanager.dto.SportRecordDTO;
+import cz.muni.fi.pa165.sportactivitymanager.service.SportActivityService;
 import cz.muni.fi.pa165.sportactivitymanager.service.SportRecordService;
 import cz.muni.fi.pa165.sportactivitymanager.service.impl.SportRecordServiceImpl;
 import java.util.AbstractList;
@@ -12,10 +13,17 @@ import java.util.ArrayList;
 import java.util.List;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
+import net.sourceforge.stripes.action.LocalizableMessage;
+import net.sourceforge.stripes.action.RedirectResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.integration.spring.SpringBean;
+import net.sourceforge.stripes.validation.Validate;
+import net.sourceforge.stripes.validation.ValidateNestedProperties;
 import net.sourceforge.stripes.validation.ValidationErrorHandler;
 import net.sourceforge.stripes.validation.ValidationErrors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -23,8 +31,12 @@ import net.sourceforge.stripes.validation.ValidationErrors;
  */
 @UrlBinding("/records/{$event}/{record.id}")
 public class RecordActionBean extends BaseActionBean implements ValidationErrorHandler {
+   final static Logger log = LoggerFactory.getLogger(RecordActionBean.class);
     
-    private List<SportRecordDTO> records = new ArrayList<SportRecordDTO>();
+    @SpringBean
+    protected SportRecordService srs;
+    
+    private List<SportRecordDTO> records ;
 
     public List<SportRecordDTO> getRecords() {
         return records;
@@ -32,11 +44,27 @@ public class RecordActionBean extends BaseActionBean implements ValidationErrorH
     
      @DefaultHandler
     public Resolution list() {
-        SportRecordService sr = new SportRecordServiceImpl();
-        records = sr.findAll();
-   
+        records = srs.findAll();   
         return new ForwardResolution("/record/list.jsp");
 
+    }
+     
+         //--- part for adding a book ----
+
+    @ValidateNestedProperties(value = {
+            @Validate(on = {"add", "save"}, field = "user", required = true),
+            @Validate(on = {"add", "save"}, field = "duration", required = true),
+            @Validate(on = {"add", "save"}, field = "distance", required = true),
+            @Validate(on = {"add", "save"}, field = "startTime", required = true)
+    })    
+    
+    private SportRecordDTO sr;
+
+    public Resolution add() {
+        log.debug("add() book={}", sr);
+        srs.create(sr);
+        //getContext().getMessages().add(new LocalizableMessage("book.add.message",escapeHTML(sr.getTitle()),escapeHTML(book.getAuthor())));
+        return new RedirectResolution(this.getClass(), "list");
     }
     
     
---
+++ sportactivitymanager-web/src/main/webapp/record/form.jsp
@@ -0,0 +1,25 @@
+<%-- 
+    Document   : form
+    Created on : 22.11.2013, 18:04:53
+    Author     : Phaser
+--%>
+<%@ taglib prefix="s" uri="http://stripes.sourceforge.net/stripes.tld" %>
+<s:errors/>
+<table>
+    <tr>
+        <th><s:label for="b1" name="record.user"/></th>
+        <td><s:text id="b1" name="record.user"/></td>
+    </tr>
+    <tr>
+        <th><s:label for="b2" name="record.duration"/></th>
+        <td><s:text id="b2" name="record.duration"/></td>
+    </tr>
+    <tr>
+        <th><s:label for="b3" name="record.distance"/></th>
+        <td><s:text id="b3" name="record.distance"/></td>
+    </tr>
+    <tr>
+        <th><s:label for="b4" name="record.startTime"/></th>
+        <td><s:text id="b4" name="record.startTime"/></td>
+    </tr>
+</table>
